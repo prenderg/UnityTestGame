82bba4f5560db1e3cc6f26ace3a75deee333cf15
P. Prendergast <p.prendergast@hotmail.com>
2014-08-19 15:43:36 -0700

    First commit of Raise the Flag Game. Test of Unity and GitHub Integration.

---
+++ Assets/Clicked.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clicked : MonoBehaviour {
+	public GameObject Cubey;
+	public float burgerMove;
+	public int jumpSize;
+	public int NoOfMoves;
+	public int moves;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown()
+	{
+		Debug.Log("you clicked");
+		if(moves<NoOfMoves)
+		{
+		Vector3 newPos = Cubey.transform.position;
+		newPos.y += burgerMove;
+		Cubey.transform.position = newPos;
+		moves+=jumpSize;
+		}
+	}
+}
---
+++ Assets/DestroyObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other) {
+		other.audio.Play ();
+		//audio.Play();
+		Destroy(gameObject);
+}
+}
